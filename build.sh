f43d919e4196d55288408f48b41214beebc7addb
Robin C. Ladiges <rcl.git@blackpinguin.de>
2021-08-28 23:01:16 +0200

    fix: different framework directory for x86

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd ./Windows/C#/HsvScreensaver/
-msbuild="/c/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe"
-$msbuild  /property:Configuration=Release  /property:Platform=x64  HsvScreensaver.csproj
-$msbuild  /property:Configuration=Release  /property:Platform=x86  HsvScreensaver.csproj
+msbuild64="/c/Windows/Microsoft.NET/Framework64/v4.0.30319/MSBuild.exe"
+msbuild86="/c/Windows/Microsoft.NET/Framework/v4.0.30319/MSBuild.exe"
+$msbuild64  /property:Configuration=Release  /property:Platform=x64  HsvScreensaver.csproj
+$msbuild86  /property:Configuration=Release  /property:Platform=x86  HsvScreensaver.csproj
